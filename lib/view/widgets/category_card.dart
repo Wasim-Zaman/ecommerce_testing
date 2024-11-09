2bc57a7131c68143be558aedf821e50a3e3dc7fb
Wasim Zaman <wasimxaman13@gmail.com>
2024-11-09 18:44:26 +0500

    Refactor README.md and pubspec.yaml files

--- lib/view/widgets/category_card.dart
+++ lib/view/widgets/category_card.dart
@@ -37,7 +37,10 @@ class CategoryCard extends StatelessWidget {
           fit: StackFit.expand,
           children: [
             // Category Image
-            const Icon(Icons.image_outlined, size: 60),
+            Image.network(
+              "https://plus.unsplash.com/premium_photo-1672883551967-ab11316526b4?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              fit: BoxFit.cover,
+            ),
             // Gradient Overlay
             Container(
               decoration: BoxDecoration(
